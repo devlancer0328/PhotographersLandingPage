45df9207d87e45008ed958a8ff8fd0d6cc5970ca
devlancer0328 <topdev0328@proton.me>
2022-10-23 10:20:31 +0200

    add global styles

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    @import url('https://fonts.googleapis.com/css2?family=Public+Sans:wght@300;400;600&display=swap');
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body {
+        font-family: 'Public Sans', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
