0acdbdbeb9976c7286897156dce8cc4d022456c8
devlancer0328 <topdev0328@proton.me>
2022-10-26 15:33:48 +0200

    add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
+import Home from "./pages/Home/Home";
 import GlobalStyle from "./styles/GlobalStyle";
 
 const App = () => {
   return ( 
     <div>
       <GlobalStyle />
-
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
     </div>
    );
 }
