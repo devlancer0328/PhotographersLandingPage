2f62510f24fb26a5000604d683adba93579528c5
devlancer0328 <topdev0328@proton.me>
2022-10-26 16:07:39 +0200

    add missing imports and Header, Footer to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import About from "./pages/about/About";
 import Contact from "./pages/contact/Contact";
 import Home from "./pages/Home/Home";
 import GlobalStyle from "./styles/GlobalStyle";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer"
 
 const App = () => {
   return ( 
     <div>
       <GlobalStyle />
+      <Header />
       <Routes>
         <Route path="home" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </div>
    );
 }
