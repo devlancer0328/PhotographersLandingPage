7b600b580f264602c50a17adb36910b318232ef1
devlancer0328 <topdev0328@proton.me>
2022-11-06 17:15:47 +0100

    add content to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,8 @@ const About = () => {
     return ( 
         <div>
             <AboutContainer>
-                About
+                <h1>Why choose <span>PhotoPassion?</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptas eligendi. Suscipit sit nihil non harum minima aspernatur natus aliquid quam accusamus neque voluptatem soluta eaque, blanditiis quo magni rerum.</p>
             </AboutContainer>
         </div>
      );
