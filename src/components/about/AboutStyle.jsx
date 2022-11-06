992006a961669e83cd8b44f74b06e4ed39e6a992
devlancer0328 <topdev0328@proton.me>
2022-11-06 17:15:56 +0100

    styling about section

--- src/components/about/AboutStyle.jsx
+++ src/components/about/AboutStyle.jsx
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div `
-
+    margin: 0 auto;
+    height: 60vh;
+    padding: 64px 16vw 64px 16vw;
+    h1 {
+        margin-bottom: 16px;
+        span {
+            font-family: 'Carter One', sans-serif;
+        }
+    }
+    p {
+        color: #888888;
+        width: 40vw;
+    }
 `;
