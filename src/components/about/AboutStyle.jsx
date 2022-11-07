f27d641b56e88f164d1d4046de1734ff5fd7766e
devlancer0328 <topdev0328@proton.me>
2022-11-07 20:18:02 +0100

    correct padding

--- src/components/about/AboutStyle.jsx
+++ src/components/about/AboutStyle.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const AboutContainer = styled.div `
     margin: 0 auto;
     height: 60vh;
-    padding: 64px 16vw 64px 16vw;
+    padding: 124px 16vw 64px 16vw;
     h1 {
         margin-bottom: 16px;
         span {
