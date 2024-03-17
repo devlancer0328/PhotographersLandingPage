b2f45fbec34f75156198116047f39b263cd7260a
devlancer0328 <topdev0328@proton.me>
2024-03-17 17:26:12 +0100

    styling gallery mobile full width and change arrow to white background

--- src/components/gallery/GalleryStyle.jsx
+++ src/components/gallery/GalleryStyle.jsx
@@ -5,7 +5,10 @@ export const GalleryContainer = styled.div`
   height: auto;
   padding: 92px 16vw 92px 16vw;
   @media (max-width: 760px) {
-    padding: 48px 32px;
+    padding: 48px 0;
+    h1 {
+      padding-left: 32px;
+    }
   }
 `;
 
@@ -15,9 +18,21 @@ export const SwiperGalleryContainer = styled.div`
   .swiper {
     height: auto;
     max-height: 600px;
-    .swiper-button-prev,
     .swiper-button-next {
       color: rgb(240, 142, 53);
+      background: #ffffff;
+      padding: 16px;
+      height: 64px;
+      width: 40px;
+      right: 0;
+    }
+    .swiper-button-prev {
+      color: rgb(240, 142, 53);
+      background: #ffffff;
+      padding: 16px;
+      height: 64px;
+      width: 40px;
+      left: 0;
     }
     .swiper-pagination {
       .swiper-pagination-bullet-active {
@@ -31,6 +46,24 @@ export const SwiperGalleryContainer = styled.div`
           object-fit: cover;
           height: 600px;
           width: 100%;
+        }
+      }
+    }
+  }
+  @media (max-width: 760px) {
+    .swiper {
+      .swiper-button-next {
+        width: 32px;
+        height: 48px;
+        &::after {
+          font-size: 28px;
+        }
+      }
+      .swiper-button-prev {
+        width: 32px;
+        height: 48px;
+        &::after {
+          font-size: 28px;
         }
       }
     }
