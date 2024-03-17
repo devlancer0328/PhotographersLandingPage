a789edab2a86b377307319874faad286d750ac99
devlancer0328 <topdev0328@proton.me>
2024-03-17 17:01:07 +0100

    add img & animations to about me section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,20 +1,54 @@
-import { AboutContainer } from "./AboutStyle";
+import { AboutContainer, ImageContainer, AboutWrapper } from "./AboutStyle";
+import cameraimg from "./../../img/about/about-me.jpg";
 
 const About = () => {
-    return ( 
-        <div id="about">
-            <AboutContainer>
-                <h1>Czemu warto wybrać <span>PhotoPassion?</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptas eligendi. Suscipit sit nihil non harum minima aspernatur natus aliquid quam accusamus neque voluptatem soluta eaque, blanditiis quo magni rerum.</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam distinctio voluptatem voluptatum repellat praesentium nulla eum possimus vel atque sunt officiis maxime minus ducimus itaque, optio accusantium quae ut error.
-                Laboriosam recusandae consectetur nesciunt sequi vel sapiente ex dicta ducimus inventore obcaecati. Temporibus praesentium facilis est labore adipisci eaque quibusdam quia voluptatem alias. Corrupti, repellendus exercitationem delectus alias labore veniam.</p>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut animi repellendus officiis adipisci quae nihil nulla eaque provident labore delectus deleniti libero cumque placeat, doloremque, vitae eum dolorum incidunt molestiae!
-                Molestias perspiciatis explicabo assumenda numquam dicta ducimus maxime, suscipit corrupti facilis hic ut esse adipisci id at, repudiandae, provident aperiam nisi molestiae. Vero dolorum repudiandae, nemo adipisci dolorem neque consequatur?
-                Quae cupiditate dolor numquam molestias iusto sint dolores modi minus voluptatum accusamus placeat molestiae, culpa enim veritatis totam laboriosam amet consectetur, quisquam praesentium inventore? Officiis earum fuga non distinctio repellat?
-                Obcaecati, voluptas. Sed deleniti ullam, itaque saepe modi necessitatibus cum sequi asperiores porro non alias impedit explicabo repellendus qui cumque voluptas quia architecto reiciendis dolorem voluptatem possimus magnam amet. Natus?</p>
-            </AboutContainer>
-        </div>
-     );
-}
+  return (
+    <div id="about">
+      <AboutWrapper>
+        <ImageContainer>
+          <img src={cameraimg} alt="camera img" />
+        </ImageContainer>
+        <AboutContainer>
+          <h1>
+            Czemu warto wybrać <span>PhotoPassion?</span>
+          </h1>
+          <p data-aos="fade-up">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi,
+            voluptas eligendi. Suscipit sit nihil non harum minima aspernatur
+            natus aliquid quam accusamus neque voluptatem soluta eaque,
+            blanditiis quo magni rerum.
+          </p>
+          <p data-aos="fade-up">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            distinctio voluptatem voluptatum repellat praesentium nulla eum
+            possimus vel atque sunt officiis maxime minus ducimus itaque, optio
+            accusantium quae ut error. Laboriosam recusandae consectetur
+            nesciunt sequi vel sapiente ex dicta ducimus inventore obcaecati.
+            Temporibus praesentium facilis est labore adipisci eaque quibusdam
+            quia voluptatem alias. Corrupti, repellendus exercitationem delectus
+            alias labore veniam.
+          </p>
+          <p data-aos="fade-up">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut animi
+            repellendus officiis adipisci quae nihil nulla eaque provident
+            labore delectus deleniti libero cumque placeat, doloremque, vitae
+            eum dolorum incidunt molestiae! Molestias perspiciatis explicabo
+            assumenda numquam dicta ducimus maxime, suscipit corrupti facilis
+            hic ut esse adipisci id at, repudiandae, provident aperiam nisi
+            molestiae. Vero dolorum repudiandae, nemo adipisci dolorem neque
+            consequatur? Quae cupiditate dolor numquam molestias iusto sint
+            dolores modi minus voluptatum accusamus placeat molestiae, culpa
+            enim veritatis totam laboriosam amet consectetur, quisquam
+            praesentium inventore? Officiis earum fuga non distinctio repellat?
+            Obcaecati, voluptas. Sed deleniti ullam, itaque saepe modi
+            necessitatibus cum sequi asperiores porro non alias impedit
+            explicabo repellendus qui cumque voluptas quia architecto reiciendis
+            dolorem voluptatem possimus magnam amet. Natus?
+          </p>
+        </AboutContainer>
+      </AboutWrapper>
+    </div>
+  );
+};
 
 export default About;
--- src/components/about/AboutStyle.jsx
+++ src/components/about/AboutStyle.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
+export const AboutWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 92px 16vw 92px 16vw;
+  @media (max-width: 760px) {
+    padding: 48px 32px;
+    flex-direction: column;
+  }
+`;
+
 export const AboutContainer = styled.div`
   margin: 0 auto;
   height: auto;
-  padding: 92px 16vw 92px 16vw;
   h1 {
     font-style: italic;
     font-weight: 600;
@@ -16,7 +26,20 @@ export const AboutContainer = styled.div`
     color: #888888;
     text-align: justify;
   }
+`;
+
+export const ImageContainer = styled.div`
+  margin: 0 40px 0 0;
+  img {
+    height: 100%;
+    width: 300px;
+    object-fit: cover;
+  }
   @media (max-width: 760px) {
-    padding: 48px 32px;
+    margin: 0 0 24px 0;
+    img {
+      width: 100%;
+      max-width: 360px;
+    }
   }
 `;
