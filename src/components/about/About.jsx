b600c9f9a9aa44c03fc98f235bcf97ee564ef4ec
devlancer0328 <topdev0328@proton.me>
2024-03-17 11:30:08 +0100

    add another sample paragraphs with width content 100%

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,6 +6,12 @@ const About = () => {
             <AboutContainer>
                 <h1>Czemu warto wybrać <span>PhotoPassion?</span></h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, voluptas eligendi. Suscipit sit nihil non harum minima aspernatur natus aliquid quam accusamus neque voluptatem soluta eaque, blanditiis quo magni rerum.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam distinctio voluptatem voluptatum repellat praesentium nulla eum possimus vel atque sunt officiis maxime minus ducimus itaque, optio accusantium quae ut error.
+                Laboriosam recusandae consectetur nesciunt sequi vel sapiente ex dicta ducimus inventore obcaecati. Temporibus praesentium facilis est labore adipisci eaque quibusdam quia voluptatem alias. Corrupti, repellendus exercitationem delectus alias labore veniam.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut animi repellendus officiis adipisci quae nihil nulla eaque provident labore delectus deleniti libero cumque placeat, doloremque, vitae eum dolorum incidunt molestiae!
+                Molestias perspiciatis explicabo assumenda numquam dicta ducimus maxime, suscipit corrupti facilis hic ut esse adipisci id at, repudiandae, provident aperiam nisi molestiae. Vero dolorum repudiandae, nemo adipisci dolorem neque consequatur?
+                Quae cupiditate dolor numquam molestias iusto sint dolores modi minus voluptatum accusamus placeat molestiae, culpa enim veritatis totam laboriosam amet consectetur, quisquam praesentium inventore? Officiis earum fuga non distinctio repellat?
+                Obcaecati, voluptas. Sed deleniti ullam, itaque saepe modi necessitatibus cum sequi asperiores porro non alias impedit explicabo repellendus qui cumque voluptas quia architecto reiciendis dolorem voluptatem possimus magnam amet. Natus?</p>
             </AboutContainer>
         </div>
      );
--- src/components/about/AboutStyle.jsx
+++ src/components/about/AboutStyle.jsx
@@ -5,7 +5,6 @@ export const AboutContainer = styled.div `
     height: auto;
     padding: 60px 16vw 64px 16vw;
     h1 {
-        margin-bottom: 16px;
         font-style: italic;
         font-weight: 600;
         span {
@@ -15,13 +14,6 @@ export const AboutContainer = styled.div `
     }
     p {
         color: #888888;
-        width: 40vw;
         text-align: justify;
-    }
-
-    @media (max-width: 1080px) {
-        p {
-            width: 100%;
-        }
     }
 `;
