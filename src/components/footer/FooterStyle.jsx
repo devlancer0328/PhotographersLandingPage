86997af95ba0b914bbaf74905b6f343270ebafc7
devlancer0328 <topdev0328@proton.me>
2024-03-17 12:30:38 +0100

    styling footer background gradient

--- src/components/footer/FooterStyle.jsx
+++ src/components/footer/FooterStyle.jsx
@@ -1,12 +1,18 @@
 import styled from "styled-components";
 
-export const FooterContainer = styled.div `
-    margin: 0 auto;
-    height: auto;
-    background-color: #333333;
-    padding: 32px 16vw 32px 16vw;
-    text-align: center;
-    p {
-        color: #FFFFFF;
-    }
+export const FooterContainer = styled.div`
+  margin: 0 auto;
+  height: auto;
+  background: linear-gradient(
+    110.78deg,
+    rgb(249, 214, 73) 15.22%,
+    rgb(240, 142, 53) 32.09%,
+    rgb(105, 105, 105) 195%
+  );
+  padding: 32px 16vw 32px 16vw;
+  text-align: center;
+  p {
+    color: #ffffff;
+    margin: 0;
+  }
 `;
